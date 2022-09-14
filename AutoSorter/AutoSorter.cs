411992d9927d4ddb4334bc16ff87b1341700b340
Pete <pete@petesplace.de>
2022-09-14 15:30:04 +0200

    fix: 1.4.1 version update

--- AutoSorter/AutoSorter.cs
+++ AutoSorter/AutoSorter.cs
@@ -22,7 +22,7 @@ namespace pp.RaftMods.AutoSorter
         /// </summary>
         public static CAutoSorter Get = null;
 
-        public const string VERSION                                 = "1.4.0";
+        public const string VERSION                                 = "1.4.1";
         public const string MOD_NAME                                = "AutoSorter";
         private const string MOD_NAMESPACE                          = "pp.RaftMods." + MOD_NAME;
 
